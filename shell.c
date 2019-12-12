d8607d18a3f5fa8a4ec7372bc48d18b861ab62df
Joseph <joseph@Aris-Air.home>
2019-12-11 21:31:30 -0500

    redir_type implemented

--- shell.c
+++ shell.c
@@ -89,6 +89,24 @@ int runCommand(char argc[256]) {
         for (int i = 0; args[i] != NULL; i ++) {
             printf("Arg: %s\n", args[i]);
         }
+
+	// Parsing arguments for a redirect character
+	char *redir_type;
+	int i = 0;
+	for(i = 0; args[i] != NULL; i++){
+	    if(strcmp(args[i], "<") == 0){
+		redir_type = args[i];
+	    } else if (strcmp(args[i], ">") == 0){
+		redir_type = args[i]; 
+	    } else if (strcmp(args[i], "<<") == 0){
+		redir_type = args[i];
+	    } else if (strcmp(args[i], ">>") == 0){
+		redir_type = args[i];
+	    } else {}
+	}
+	printf("Redirect Character: %s", redir_type);
+	
+	
         execvp(args[0], args);
     }
     else if (f ==-1) {
